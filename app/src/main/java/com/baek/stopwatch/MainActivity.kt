a15847ab2acf435c1af105fcd097891ed042cc78
BaekDorazi <qorehgud@gmail.com>
2020-02-22 17:06:53 +0900

    기능추가, UI수정 / reset버튼 기능 추가, 랩 타임 버튼 기능 추가, FloatingActionButton 색 변경

--- app/src/main/java/com/baek/stopwatch/MainActivity.kt
+++ app/src/main/java/com/baek/stopwatch/MainActivity.kt
@@ -2,18 +2,25 @@ package com.baek.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     private var time = 0
     private var timeTask: Timer? = null
     private var isRunning = false
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabReset.setOnClickListener {
+            reset()
+        }
 
         fabPlayPause.setOnClickListener {
             isRunning = !isRunning
@@ -23,6 +30,10 @@ class MainActivity : AppCompatActivity() {
             } else {
                 pause()
             }
+        }
+
+        btnLab.setOnClickListener {
+            recordLapTime()
         }
     }
 
@@ -45,5 +56,27 @@ class MainActivity : AppCompatActivity() {
         fabPlayPause.setImageResource(R.drawable.ic_play_arrow_black_24dp)
 
         timeTask?.cancel()
+    }
+
+    private fun recordLapTime() {
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAB: ${lapTime / 100}.${lapTime % 100}"
+
+        lapLayout.addView(textView, 0)
+        lap++
+    }
+
+    private fun reset() {
+        timeTask?.cancel()
+
+        time = 0
+        isRunning = false
+        fabPlayPause.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        tvSec.text = "0"
+        tvMilliSec.text = "00"
+
+        lapLayout.removeAllViews()
+        lap = 1
     }
 }
